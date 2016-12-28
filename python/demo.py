574a4202ec33877e16d0358a6620b01719673398
Mike C. Fletcher <mcfletch@vrplumber.com>
2016-12-27 22:48:12 -0500

    Show full program number and PID setting (though not actually needed in the HLS context)

--- python/demo.py
+++ python/demo.py
@@ -7,14 +7,31 @@ from gi.repository import GLib
 from gi.repository import Gst
 log = logging.getLogger('gstreamer-demo')
 
+class ComponentNamespace( object ):
+    def __init__(self,pipeline):
+        self.pipeline = pipeline 
+    def __getattribute__(self, name ):
+        if name != 'pipeline':
+            return self.pipeline.get_child_by_name( name )
+        return super(ComponentNamespace,self).__getattribute__(name)
+
 class Pipe( object ):
     """Instance that runs a pipeline for us"""
-    def __init__( self, pipe ):
+    def __init__( self, name, pipe ):
+        self.name = name
         self.pipeline_command = pipe 
         self.pipeline = None
         self.state = Gst.State.NULL
     def run( self ):
-        self.pipeline = Gst.parse_launchv( self.pipeline_command )
+        try:
+            self.pipeline = Gst.parse_launchv( self.pipeline_command )
+        except GLib.Error as err:
+            log.error("Failed to parse the pipeline: %s", err)
+            log.info(
+                " gst-launch --gst-debug=3 %s",
+                " ".join( self.pipeline_command )
+            )
+            raise
         bus = self.pipeline.get_bus()
         bus.add_signal_watch()
         bus.connect( "message", self.message )
@@ -28,42 +45,84 @@ class Pipe( object ):
             return method( bus, message )
         else:
             log.info("message: %s", message.type )
+    @property
+    def components(self):
+        return ComponentNamespace( self.pipeline )
     def message_error(self, bus, message ):
         err, debug = message.parse_error()
         log.error( "Error reported, aborting: %s (debug=%s)", err, debug )
         LOOP.quit()
+    def message_state_changed(self, bus, message ):
+        structure = message.parse_state_changed()
+        newstate = structure.newstate
+        if newstate != self.state:
+            log.info('%s state: %s -> %s', self.name, self.state, newstate )
+            self.state = newstate
+    def message_stream_start(self, bus, message ):
+        log.info("Stream started")
+        for i,pad in enumerate(self.components.muxer.sinkpads):
+            log.info("Muxer pad: %s", pad.name)
 
 SAMPLE_PIPELINE = [
     # generate some video test output
-    'videotestsrc', 'pattern=0', '!',
+    'videotestsrc', 'name=videotest', 'pattern=0', '!',
     # make it NTSC SD content
     'video/x-raw,framerate=30000/1001,width=720,height=480', '!',
     # encode with x264 (note: you may require a license to deploy this!)
     # Note that *this* element uses kbps for bitrate, some use bps
-    'x264enc', 'bitrate=256', '!' 
-    # package into an MPEG TS container format
-    'mpegtsmux', 'name=muxer', '!',
+    'x264enc', 'bitrate=256', '!', 
+    # Send it to the MPEG TS muxer we'll define later with a PID 
+    # of 65 (the sink name determines the PID)
+    'muxer.sink_65',
+    # Note that there is *no* '!' here! The next segment is a separate 
+    # sub-pipeline that we're going to link by name...
+    
+    # package all streams into an MPEG TS container format
+    'mpegtsmux', 
+        # we use this name in the other pipeline fragments to route 
+        # the content into this particular element...
+        'name=muxer', 
+        # silly example, here we say video (65) and audio (66) should 
+        # be mapped into the same program with program number 10 (instead 
+        # of the default program number 1). This isn't in any way necessary,
+        # it just shows how to use "GstStructure" instances...
+        # The sink-name were defined in the pipe operations that sent the 
+        # content into the muxer...
+        'prog-map=program_map,sink_65=10,sink_66=10',
+        # Note that currently we have no way to specify the 
+        # PIDs on the TS outputs, not a concern for HLS, but 
+        # a concern for multicast video setups...
+        '!',
     # dump to an HLS playlist/video-file...
     'hlssink',
         'location=/var/www/segment-%05d.ts',
         'playlist-location=/var/www/index.m3u8',
         'max-files=20',
-        'target-duration=2',
+        'target-duration=15',
     # setup a parallel pipeline for audio...
     'audiotestsrc', '!',
     # parameters are high-quality monoaural audio
     'audio/x-raw,rate=48000,channels=1', '!',
     # encoded as AAC for compatibility with HLS
     'avenc_aac', 'bitrate=32000', '!',
-    'muxer.',
+    # again, send it into the muxer, this time on sink/pid 66
+    'muxer.sink_66',
 ]
         
 def main():
     global LOOP
     LOOP = GObject.MainLoop()
     Gst.init_check(None)
-    pipe = Pipe( SAMPLE_PIPELINE )
+    pipe = Pipe( 'sample',SAMPLE_PIPELINE )
     pipe.run()
+    def rotate_pattern(*args,**named):
+        videotest = pipe.components.videotest
+        pattern = videotest.get_property( 'pattern' )
+        pattern = (pattern + 1)%24
+        videotest.set_property( 'pattern', pattern )
+        GObject.timeout_add( 100, rotate_pattern )
+    rotate_pattern()
+    log.info("Muxer: %s", pipe.components.muxer)
     LOOP.run()
 
 if __name__ == "__main__":
